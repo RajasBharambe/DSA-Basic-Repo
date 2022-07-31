09919a18d748d6ce4d645e08247db7ce2c2f1696
Rajas Bharambe <89535670+RajasBharambe@users.noreply.github.com>
2022-07-31 20:54:38 +0530

    Update ShellSort.cpp

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -3,7 +3,21 @@ using namespace std;
 
 void ShellSort(int arr[], int n)
 {
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int j = gap; j < n; j += 1)
+        {
+            int temp = arr[j];
+            int i = 0;
 
+            for (i = j; (i >= gap) && (arr[i - gap] > temp); i -= gap)
+            {
+                arr[i] = arr[i - gap];
+            }
+            arr[i] = temp;
+        }
+    }
+}
 
 int main()-
 {
