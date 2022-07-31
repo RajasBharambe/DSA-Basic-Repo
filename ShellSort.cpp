bc186c60fe8a00b5587915840c521487f9203a76
Rajas Bharambe <89535670+RajasBharambe@users.noreply.github.com>
2022-07-31 20:55:12 +0530

    Update ShellSort.cpp

--- ShellSort.cpp
+++ ShellSort.cpp
@@ -19,22 +19,7 @@ void ShellSort(int arr[], int n)
     }
 }
 
-int main()-
-{
-    int n;
-    cout << "Enter size of the array: " << endl;
-    cin >> n;
-    int arr1[n];
-    cout << "Enter " << n << " integers in any order" << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cin >> arr1[i];
-    }
-    cout << "Before Sorting: " << endl;
-    for (int i = 0; i < n; i++)
-    {
-        cout << arr1[i] << " ";
-    }
+
 
     cout << endl
          << "Shell Sort";
